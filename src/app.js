ee4a11de2e37de0349df1c8efe93877a106f9303
Aniebiet (mega) ifiok <140168380+mega-ifiok001@users.noreply.github.com>
2026-02-05 16:32:58 +0100

    add a codesapce link

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./config/passport.js"
 import authRoutes from "./modules/auth/auth.routes.js"
 
 const app = express()
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
+app.use(cors({ origin: process.env.CODESPACE_URL, credentials: true }))
 app.use(express.json())
 app.use(passport.initialize())
 
