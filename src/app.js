c270670d4893e8a40f33c88621fa677d119cfecc
Aniebiet (mega) ifiok <140168380+mega-ifiok001@users.noreply.github.com>
2026-02-05 16:38:20 +0100

    removed cors strict mode

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./config/passport.js"
 import authRoutes from "./modules/auth/auth.routes.js"
 
 const app = express()
-app.use(cors({ origin: process.env.CODESPACE_URL, credentials: true }))
+app.use(cors())
 app.use(express.json())
 app.use(passport.initialize())
 
